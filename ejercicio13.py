f1f90075eca27f91cf044b926e33e63274c277e0
edercarmona <edercarmona_22@hotmail.com>
2019-08-07 06:22:43 -0500

    Presentacion

--- ejercicio13.py
+++ ejercicio13.py
@@ -10,7 +10,7 @@ cap = cv2.VideoCapture(0)
 while(True):
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray)
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(125,255,0),2)
     cv2.imshow('img',img)
